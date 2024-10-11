58c5e466f5634d479588d41a7de252070f70d463
KhangDang <102233798+khangdang12324@users.noreply.github.com>
2024-10-11 10:55:33 +0700

    Add files via upload

---
+++ Lab6_2212387/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Lab6_2212387
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.lvCatagory = new System.Windows.Forms.ListView();
+            this.chID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chType = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lvb = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.txtType = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnADD = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoad.Location = new System.Drawing.Point(12, 157);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(147, 41);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Lấy danh sách";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // lvCatagory
+            // 
+            this.lvCatagory.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chID,
+            this.chName,
+            this.chType});
+            this.lvCatagory.FullRowSelect = true;
+            this.lvCatagory.HideSelection = false;
+            this.lvCatagory.Location = new System.Drawing.Point(12, 204);
+            this.lvCatagory.MultiSelect = false;
+            this.lvCatagory.Name = "lvCatagory";
+            this.lvCatagory.Size = new System.Drawing.Size(1393, 396);
+            this.lvCatagory.TabIndex = 1;
+            this.lvCatagory.UseCompatibleStateImageBehavior = false;
+            this.lvCatagory.View = System.Windows.Forms.View.Details;
+            this.lvCatagory.Click += new System.EventHandler(this.lvCatagory_Click);
+            // 
+            // chID
+            // 
+            this.chID.Text = "Mã loại";
+            this.chID.Width = 369;
+            // 
+            // chName
+            // 
+            this.chName.Text = "Tên loại món ăn";
+            this.chName.Width = 555;
+            // 
+            // chType
+            // 
+            this.chType.Text = "Loại";
+            this.chType.Width = 1251;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtID);
+            this.panel1.Controls.Add(this.lvb);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(382, 33);
+            this.panel1.TabIndex = 2;
+            // 
+            // lvb
+            // 
+            this.lvb.AutoSize = true;
+            this.lvb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvb.Location = new System.Drawing.Point(17, 4);
+            this.lvb.Name = "lvb";
+            this.lvb.Size = new System.Drawing.Size(79, 20);
+            this.lvb.TabIndex = 0;
+            this.lvb.Text = "Mã nhóm:";
+            // 
+            // txtID
+            // 
+            this.txtID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtID.Location = new System.Drawing.Point(102, 4);
+            this.txtID.Name = "txtID";
+            this.txtID.ReadOnly = true;
+            this.txtID.Size = new System.Drawing.Size(277, 26);
+            this.txtID.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txtName);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(13, 54);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(382, 33);
+            this.panel2.TabIndex = 3;
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(160, 4);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(219, 26);
+            this.txtName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên nhóm thức ăn:";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.txtType);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Location = new System.Drawing.Point(13, 93);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(382, 33);
+            this.panel3.TabIndex = 3;
+            // 
+            // txtType
+            // 
+            this.txtType.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtType.Location = new System.Drawing.Point(102, 4);
+            this.txtType.Name = "txtType";
+            this.txtType.Size = new System.Drawing.Size(277, 26);
+            this.txtType.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(17, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Loại:";
+            // 
+            // btnADD
+            // 
+            this.btnADD.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnADD.Location = new System.Drawing.Point(808, 148);
+            this.btnADD.Name = "btnADD";
+            this.btnADD.Size = new System.Drawing.Size(147, 41);
+            this.btnADD.TabIndex = 4;
+            this.btnADD.Text = "Thêm";
+            this.btnADD.UseVisualStyleBackColor = true;
+            this.btnADD.Click += new System.EventHandler(this.btnADD_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Enabled = false;
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(1258, 148);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(147, 41);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Xóa";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Enabled = false;
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(1033, 148);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(147, 41);
+            this.btnUpdate.TabIndex = 6;
+            this.btnUpdate.Text = "Cập nhật";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1417, 612);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnADD);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lvCatagory);
+            this.Controls.Add(this.btnLoad);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "b";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.ListView lvCatagory;
+        private System.Windows.Forms.ColumnHeader chID;
+        private System.Windows.Forms.ColumnHeader chName;
+        private System.Windows.Forms.ColumnHeader chType;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label lvb;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox txtType;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnADD;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
---
+++ Lab6_2212387/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab6_2212387
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            string connectString = @"Data Source=PC743;Initial Catalog=RestaurantManagement;Integrated Security=True;";
+            SqlConnection sqlConnection = new SqlConnection(connectString);
+
+            SqlCommand command = sqlConnection.CreateCommand();
+
+            string query = "SELECT ID, Name, Type FROM Category";
+            command.CommandText = query;
+            sqlConnection.Open();
+
+            SqlDataReader sqlDataReader = command.ExecuteReader();
+
+            this.DisplayCategory(sqlDataReader);
+
+            sqlConnection.Close();
+        }
+
+        private void DisplayCategory(SqlDataReader reader)
+        {
+            lvCatagory.Items.Clear();
+            while (reader.Read())
+            {
+                ListViewItem item = new ListViewItem(reader["ID"].ToString());
+
+                lvCatagory.Items.Add(item);
+
+                item.SubItems.Add(reader["Name"].ToString());
+                item.SubItems.Add(reader["Type"].ToString());
+            }
+        }
+
+        private void btnADD_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtName.Text) || string.IsNullOrWhiteSpace(txtType.Text))
+            {
+                MessageBox.Show("Vui lòng nhập tên và loại.");
+                return;
+            }
+
+            string connectString = @"Data Source=PC743;Initial Catalog=RestaurantManagement;Integrated Security=True;";
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectString))
+            {
+                sqlConnection.Open();
+                using (SqlCommand sqlcommand = sqlConnection.CreateCommand())
+                {
+                    sqlcommand.CommandText = "INSERT INTO Category(Name, [Type]) VALUES (@Name, @Type)";
+                    sqlcommand.Parameters.AddWithValue("@Name", txtName.Text);
+                    sqlcommand.Parameters.AddWithValue("@Type", txtType.Text);
+
+                    try
+                    {
+                        int numOfRowsEffected = sqlcommand.ExecuteNonQuery();
+
+                        if (numOfRowsEffected == 1)
+                        {
+                            MessageBox.Show("Thêm nhom món thành công");
+                            btnLoad.PerformClick();
+                            txtName.Text = "";
+                            txtType.Text = "";
+                        }
+                        else
+                        {
+                            MessageBox.Show("Da co loi xay ra. Vui long thu lai!");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"An error occurred: {ex.Message}");
+                    }
+                }
+            }
+        }
+
+        private void lvCatagory_Click(object sender, EventArgs e)
+        {
+            ListViewItem item = lvCatagory.SelectedItems[0];
+
+            txtID.Text = item.Text;
+            txtName.Text = item.SubItems[1].Text;
+            txtType.Text = item.SubItems[1].Text == "0" ? "Thức uống" : "Đồ ăn";
+
+
+            btnUpdate.Enabled = true;
+            btnDelete.Enabled = true;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string connectString = @"Data Source=PC743;Initial Catalog=RestaurantManagement;Integrated Security=True;";
+            SqlConnection sqlConnection = new SqlConnection(connectString);
+
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+
+            sqlCommand.CommandText = "UPDATE Category SET Name = N'" + txtName.Text +
+                "', [Type] = " + txtType.Text + "WHERE ID = " + txtID.Text;
+
+            sqlConnection.Open();
+
+            int numOfRowsEffected = sqlCommand.ExecuteNonQuery();
+
+            sqlConnection.Close();
+
+            if ( numOfRowsEffected == 0)
+            {
+                ListViewItem item = lvCatagory.SelectedItems[0];
+
+                item.SubItems[1].Text = txtName.Text;
+                item.SubItems[2].Text = txtType.Text;
+
+                txtID.Text = string.Empty; txtName.Text = string.Empty;txtType.Text = string.Empty;
+
+
+                btnUpdate.Enabled = false;
+                btnDelete.Enabled = false;
+
+                MessageBox.Show("Cap nhat nhom mon an thanh cong");
+            }
+            else
+            {
+                MessageBox.Show("DA co loi");
+            }
+        }
+    }
+
+}
